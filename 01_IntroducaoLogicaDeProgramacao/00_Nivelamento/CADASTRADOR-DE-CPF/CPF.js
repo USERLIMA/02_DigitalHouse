be1b4585c060a19b8fc353441a6a7dc2a7ddbf1e
CAIO LIMA SOUSA <caiolgn88@gmail.com>
2022-10-03 16:36:33 -0300

    CADASTRADOR-DE-CPF

--- 01_IntroducaoLogicaDeProgramacao/00_Nivelamento/CADASTRADOR-DE-CPF/CPF.js
+++ 01_IntroducaoLogicaDeProgramacao/00_Nivelamento/CADASTRADOR-DE-CPF/CPF.js
@@ -1,8 +1,10 @@
+//console.table(["Caio", "Lima", "Sousa"])
 // 1 - Capturar o CPF digitado
 
 let cpfDigitado = process.argv[2];
 
 // 2 - Verificar a validade do CPF
+
 const cpf = require('cpf');
 
 //  2a - Se for válido, salvar o cpf num arquivo
@@ -13,11 +15,11 @@ if(ehValido){
     console.log(`${cpfDigitado} é um CPF válido. Salvando...`);
     
     //Importar a biblioteca FS (biblioteca de manipulação de arquivos e pastas);
-    //const fs = require('fs');
-    //fs.appendFileSync('historico-de-cpfs.txt', cpfDigitado);
+    const fs = require('fs');
+    fs.appendFileSync('historico-de-cpfs.txt', cpfDigitado);
     //Agora vamos importar a função cpf e depois salvar 
-    const salvarCpf = require('./functions/salvarCpf.js.js')
-    salvarCpf(cpfDigitado)
+    //const salvarCpf = require('./functions/salvarCpf.js.js')
+    //salvarCpf(cpfDigitado)
 
 }else{
     //  2b - Se não for válido, emitir mensagem de erro
