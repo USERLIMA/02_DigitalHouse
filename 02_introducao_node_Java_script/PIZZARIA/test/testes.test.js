ecb5bf7e736d2acae69a855998b1113c44518e65
CAIO LIMA SOUSA <caiolgn88@gmail.com>
2023-02-05 13:34:06 -0300

    ajustando pastas

---
+++ 02_introducao_node_Java_script/PIZZARIA/test/testes.test.js
@@ -0,0 +1,13 @@
+const usuariosServices = require('../services/UsuariosServices')
+
+UsuariosServices.alterarFormaDePagamento('asdasd',1,1)
+
+// ----------------------------------------------------------------------
+
+let nome = "Caio";
+let dataDeNascimento = "2000-04-22";
+
+const pessoa = {
+    nome : nome,
+    dataDeNascimento: dataDeNascimento
+}
--- 02_introducao_node_Java_script/PIZZARIA/testes.test.js
+++
@@ -1,5 +0,0 @@
-const usuariosServices = require('./services/UsuariosServices')
-
-UsuariosServices.alterarFormaDePagamento('asdasd',1,1)
-
-// escreva um códico em javascript para conexao com sql 
