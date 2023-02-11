279ec3922c7825d43be737973de18253fdd99fc7
CAIO LIMA SOUSA <caiolgn88@gmail.com>
2023-02-11 13:33:40 -0300

    testes com array 2

---
+++ 02_introducao_node_Java_script/PIZZARIA/test/usuarios-listar.test.js
@@ -0,0 +1,5 @@
+const usuariosServices = require('../services/UsuariosServices');
+
+usuariosServices.listar();
+
+// ----------------------------------------------------------------------
---
+++ 02_introducao_node_Java_script/PIZZARIA/test/usuarios-salvar.test.js
@@ -0,0 +1,17 @@
+const usuariosServices = require('../services/UsuariosServices');
+
+usuariosServices.salvar
+(
+    [
+    {
+        nome:"Caio",
+        idade: 22
+    },
+    {
+        nome:"Val",
+        idade: 53
+    },
+    ]
+);
+
+// ----------------------------------------------------------------------
